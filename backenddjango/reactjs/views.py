8cbaff5fcd7a0d81b737ee6a1c9277443bddbcc5
atanozone <42525994+atanozone@users.noreply.github.com>
2018-10-14 15:44:42 +0800

    resloved python env error

--- backenddjango/reactjs/views.py
+++ backenddjango/reactjs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#render() finds template/ directory
+#redirect all request to appdirectory/template/frontendreact/index.html
+def index(request):
+    return render(request, 'reactjs/index.html')
--- projectenv-lib/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ozon/git/fyp-2018-django/projectenv-lib/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
