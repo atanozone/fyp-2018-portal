52ca01d486e3430c8d1fb54f3b6b7e50615fb6b2
atanozone <42525994+atanozone@users.noreply.github.com>
2018-10-14 19:21:03 +0800

    temporary fix to webpack chunk

--- backenddjango/reactjs/reactproject/config/paths.js
+++ backenddjango/reactjs/reactproject/config/paths.js
@@ -42,7 +42,7 @@ function getServedPath(appPackageJson) {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('.../assets/bundles/'),
+  appBuild: resolveApp('../../assets/bundles/'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveApp('src/index.js'),
@@ -54,5 +54,5 @@ module.exports = {
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json')),
-  statsRoot: resolveApp('.../'),
+  statsRoot: resolveApp('../../'),
 };
